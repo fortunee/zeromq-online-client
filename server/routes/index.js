e42ace6914c714f447d71d8018fe71d75cc81c8c
Fortune Ekeruo <legalmody@gmail.com>
2018-04-30 15:34:57 +0100

    Add a req pipeline pattern

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ import pub from './pub.routes';
 import sub from './sub.routes';
 import push from './push.routes';
 import pull from './pull.routes';
+import request from './req.routes';
 
 const routes = (router) => {
   pub(router);
   sub(router);
   push(router);
   pull(router);
+  request(router);
 };
 
 export default routes;
